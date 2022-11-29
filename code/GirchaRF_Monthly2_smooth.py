bba1c47fb25c6272363953439c247997440998e5
Nebiyu Waliyi Tekesa <118362720+NebiyuWaliyi@users.noreply.github.com>
2022-11-29 00:14:20 -0800

    Add files via upload

---
+++ code/GirchaRF_Monthly2_smooth.py
@@ -0,0 +1,49 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Nov  8 10:42:08 2022
+
+@author: Nebiyu
+"""
+
+import matplotlib.pyplot as plt
+from scipy.interpolate import make_interp_spline
+%matplotlib
+import numpy as np
+import pandas as pd
+
+Smooth=pd.read_excel('output/GirchaSmooth.xlsx', sheet_name='Sheet1')
+#Line plot
+fig,ax=plt.subplots()
+ax.plot(Smooth.Date,Smooth.Hist, c='k',ls='--',label='Hist')
+ax.plot(Smooth.Date,Smooth.Rain45,c='k',ls='--',label='RCP4.5')
+ax.plot(Smooth.Date,Smooth.Rain85,c='red',label='RCP8.5')
+ax.plot(Smooth.Date,Smooth.Obs,c='b',label='Obs')
+ax.set_xlabel('Month')
+ax.set_ylabel('Total Monthly Rainfall (mm)')
+ax.set_title('Gircha Projected Rainfall for CCLM4 model')
+fig.legend(loc='upper right')
+fig.savefig('output/Projected_Gircha_Monthly1_line.png', dpi=120)
+plt.show()
+
+#smooth line
+X_Y_SplineH = make_interp_spline(Smooth.Date,Smooth.Hist)
+X_Y_SplineO = make_interp_spline(Smooth.Date,Smooth.Obs)
+X_Y_Spline45 = make_interp_spline(Smooth.Date,Smooth.Rain45)
+X_Y_Spline85 = make_interp_spline(Smooth.Date,Smooth.Rain85)
+x=np.linspace(Smooth.Date.min(),Smooth.Date.max())
+yH=X_Y_SplineH(x)
+yO=X_Y_SplineO(x)
+y45=X_Y_Spline45(x)
+y85=X_Y_Spline85(x)
+fig,ax=plt.subplots()
+ax.plot(x,yO,c='b',label='Obs')
+ax.plot(x,yH, c='green', ls='-.',label='Hist')
+ax.plot(x,y45, c='k',ls='--',label='RCP4.5')
+ax.plot(x,y85, c='red',label='RCP8.5')
+ax.set_xlabel('Month')
+ax.set_ylabel('Total Monthly average Rainfall (mm)')
+ax.set_title('Gircha Projected Rainfall for CCLM4 model')
+plt.legend()
+fig.savefig('output/Projected_Gircha_Monthly1_smooth.png', dpi=120)
+plt.show()
+
